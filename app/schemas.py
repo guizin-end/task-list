8f9bdb99ce979624e8bdb0eabe934a8e9f1caa56
Guilherme Almeida <gvba.1999@gmail.com>
2026-02-23 18:17:27 -0300

    Fixed update_user to properly receive optional fields

--- app/schemas.py
+++ app/schemas.py
@@ -11,3 +11,9 @@ class UserPublic(BaseModel):
     id: str
     username: str
     email: EmailStr
+
+
+class UserUpdate(BaseModel):
+    username: str | None = None
+    email: EmailStr | None = None
+    password: str | None = None
