312c6b6a3ae3f835b20d69f6ba4ac0a9567f4331
Guilherme Almeida <gvba.1999@gmail.com>
2026-02-25 14:14:02 -0300

    Add .env.example and change DATABASE_URL to be mounted inside compose.yaml

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -11,7 +12,8 @@ from app.settings import get_settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', get_settings().DATABASE_URL)
+url = os.environ.get("DATABASE_URL")
+config.set_main_option('sqlalchemy.url', url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
