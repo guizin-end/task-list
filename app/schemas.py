43ad4088325761ae2f0fbab683a352e21a52bd8c
Guilherme Almeida <gvba.1999@gmail.com>
2026-03-01 17:09:39 -0300

    Add schemas for todos

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel, EmailStr
 
+from app.models import TodoStatus
+
 
 class UserSchema(BaseModel):
     username: str
@@ -17,3 +19,18 @@ class UserUpdate(BaseModel):
     username: str | None = None
     email: EmailStr | None = None
     password: str | None = None
+
+
+class TodoSchema(BaseModel):
+    title: str
+    description: str
+
+
+class TodoPublic(TodoSchema):
+    id: str
+    status: TodoStatus
+
+
+class TodoUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
